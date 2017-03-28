3c5109666f444dfeaae69a62322010ab1eebbe88
Devshi Mehrotra <devshim12@gmail.com>
2017-03-28 17:42:59 -0500

    dfs

--- services/wit.js
+++ services/wit.js
@@ -84,3 +84,23 @@ if (require.main === module) {
 	client.interactive()
 }
 
+// GET WEATHER FROM API
+var getWeather = function (location) {
+	return new Promise(function (resolve, reject) {
+		var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ location +'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+		request(url, function (error, response, body) {
+		    if (!error && response.statusCode == 200) {
+		    	var jsonData = JSON.parse(body)
+		    	var forecast = jsonData.query.results.channel.item.forecast[0].text
+		      console.log('WEATHER API SAYS....', jsonData.query.results.channel.item.forecast[0].text)
+		      return forecast
+		    }
+			})
+	})
+}
+
+// CHECK IF URL IS AN IMAGE FILE
+var checkURL = function (url) {
+    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
+
